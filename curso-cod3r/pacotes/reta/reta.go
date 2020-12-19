6cd25495e8f9c407866f405da724a4195a26b9ba
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2020-12-19 14:50:00 -0300

    Capitulo de pacotes

---
+++ curso-cod3r/pacotes/reta/reta.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+// Iniciado com letra maiúscula é PÚBLICO (vísivel dentro e fora do pacote)
+// Iniciando com letra minuscula é PRIVADO (visível apenas dentro do pacote)
+
+// Por exemoplo
+// Ponto - gerará algo publico
+// ponto ou _Ponto - gerará algo privado
+
+// Ponto representa uma coordenada no plano cartesiano
+type Ponto struct {
+	x float64
+	y float64
+}
+
+func catetos(a, b Ponto) (cx, cy float64) {
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+	return
+}
+
+// Distancia é responsável por calcular a distância linear entre dois pontos
+func Distancia(a, b Ponto) float64 {
+	cx, cy := catetos(a, b)
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
---
+++ curso-cod3r/pacotes/usandolib/usandolib.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gislainy/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+}
